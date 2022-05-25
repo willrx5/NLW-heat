3bb4e11fc24d060a959ef8959d932618d31057d6
Wilian <williamrx5@gmail.com>
2022-05-25 15:20:38 -0300

    add source code

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = { 
+    github : "willrx5",
+    youtube : "william",
+    facebook : "william",
+    instagram: "william",
+    twitter: "william"
+}  
+function changeSocialMediaLinks(){
+        for ( let li of socialLinks.children){
+            const social = li.getAttribute('class')
+            li.children[0].href= `https://${social}.com/${LinksSocialMedia[social]}`
+        }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos(){
+    const url  = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+    fetch(url)
+    .then(response =>  response.json()) 
+    .then(data => { 
+        userName.textContent=  data.name 
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        userPhoto.src = data.avatar_url
+        userLogin.textContent = data.login
+    })
+}
+
+
+getGitHubProfileInfos()
+
